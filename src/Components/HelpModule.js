3ee009188b107f787e453595439d8f9a96372fd0
Sumit Jangir (XSUMJ) <XSUMJ@saxobank.com>
2018-05-04 13:09:09 +0530

    come changes

--- src/Components/HelpModule.js
+++ src/Components/HelpModule.js
@@ -11,6 +11,11 @@ class HelpModule extends React.Component {
                 <h4>
                     How to use this Application -
 </h4>
+                <p><strong>Start Client side server</strong></p>
+                <p>1. Clone the repository at local machine</p>
+                <p>2. Open Command prompt and run command : npm install, to load all dependencies.</p>
+                <p>3. Run command : npm install -g json-server, to install json-server package to host REST API's locally.</p>
+                <p></p>
                 <p><strong>Start DB server Server</strong></p>
                 <p>1. Open command prommt and change directory to where you cloned this repository.</p>
                 <p>2. execute this command : json-server -p 4000 EmployeeDb.json</p>

7cb5cf5f55b3af95b742142814accd040054ff39
Sumit Jangir (XSUMJ) <XSUMJ@saxobank.com>
2018-05-14 18:39:42 +0530

    combining server and client server in single process

---
+++ server.js
@@ -0,0 +1,11 @@
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('EmployeeDB.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(4000, () => {
+  console.log('JSON Server is running')
+})
